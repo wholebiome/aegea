78365a6e9e2e6f4cbc9c3aeec7499469f3c05798
Andrey Kislyuk <kislyuk@gmail.com>
2016-03-08 07:52:00 -0800

    WIP

---
+++ aegea/rm.py
@@ -0,0 +1,23 @@
+import os, sys, argparse, subprocess
+import boto3
+from . import register_parser
+
+def rm(args):
+    for name in args.names:
+        if name.startswith("sg-"):
+            boto3.resource("ec2").SecurityGroup(name).delete()
+        elif name.startswith("vol-"):
+            boto3.resource("ec2").Volume(name).delete()
+        elif name.startswith("snap-"):
+            boto3.resource("ec2").Snapshot(name).delete()
+        elif name.startswith("ami-"):
+            image = boto3.resource("ec2").Image(name)
+            snapshot_id = image.block_device_mappings[0].get("Ebs", {}).get("SnapshotId")
+            image.deregister()
+            if snapshot_id:
+                boto3.resource("ec2").Snapshot(snapshot_id).delete()
+        elif name.startswith("sir-"):
+            boto3.client("ec2").cancel_spot_instance_requests(SpotInstanceRequestIds=[name])
+
+parser = register_parser(rm, help='Remove resources')
+parser.add_argument('names', nargs='+')
