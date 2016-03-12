f74444fd31452ffd0f9021a2b0ad1a8283373811
Andrey Kislyuk <kislyuk@gmail.com>
2016-03-12 12:03:38 -0800

    WIP

--- aegea/util/compat.py
+++ aegea/util/compat.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import os, sys
+
 USING_PYTHON2 = True if sys.version_info < (3, 0) else False
 
 if USING_PYTHON2:
