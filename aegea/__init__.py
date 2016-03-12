8d481275143c4c1eac60f5261d2003b92cbf7d16
Andrey Kislyuk <kislyuk@gmail.com>
2016-03-12 09:28:02 -0800

    WIP

--- aegea/__init__.py
+++ aegea/__init__.py
@@ -4,15 +4,20 @@ Amazon Web Services Operator Interface
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import argparse, logging
+import os, sys, argparse, logging, shutil
 
 from tweak import Config
 
 from .util.printing import BOLD, RED, ENDC
 
-config = Config(__name__, use_yaml=True, save_on_exit=False)
-
 logger = logging.getLogger(__name__)
+
+config = Config(__name__, use_yaml=True, save_on_exit=False)
+if not os.path.exists(config._config_file):
+    config.save()
+    shutil.copy(os.path.join(os.path.dirname(__file__), "default_config.yml"), config._config_file)
+    logger.info("Wrote new config file %s with default values", config._config_file)
+    config = Config(__name__, use_yaml=True, save_on_exit=False)
 
 parser = argparse.ArgumentParser(description="{}: {}".format(BOLD() + RED() + __name__.capitalize() + ENDC(), __doc__))
 parser.set_defaults(entry_point=lambda args: parser.print_help())
