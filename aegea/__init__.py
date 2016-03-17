e1188b9375980ded46fe3ff395f91ec121e3e2ba
Andrey Kislyuk <kislyuk@gmail.com>
2016-03-17 13:00:34 -0700

    WIP

--- aegea/__init__.py
+++ aegea/__init__.py
@@ -20,11 +20,11 @@ if not os.path.exists(config._config_file):
     config = Config(__name__, use_yaml=True, save_on_exit=False)
 
 parser = argparse.ArgumentParser(description="{}: {}".format(BOLD() + RED() + __name__.capitalize() + ENDC(), __doc__))
-parser.set_defaults(entry_point=lambda args: parser.print_help())
+# FIXME parser.set_defaults(entry_point=lambda args: parser.print_help())
 subparsers = parser.add_subparsers(title='commands')
 
 def register_parser(function, **kwargs):
     parser = subparsers.add_parser(function.__name__, **kwargs)
     parser.add_argument("--max-col-width", "-w", type=int, default=32)
-    # FIXME parser.set_defaults(entry_point=function)
+    parser.set_defaults(entry_point=function)
     return parser
