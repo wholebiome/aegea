9e5795babb0408f6d479c46ae4bb2cf31da89730
Andrey Kislyuk <kislyuk@gmail.com>
2016-03-17 07:19:38 -0700

    fix test

--- aegea/top.py
+++ aegea/top.py
@@ -12,10 +12,10 @@ def top(args):
     table = []
     columns = ["Region", "Instances", "AMIs"]
     for region in boto3.Session().get_available_regions("ec2"):
-        session = boto3.Session(region_name=region["RegionName"])
+        session = boto3.Session(region_name=region)
         num_instances = len(list(session.resource("ec2").instances.all()))
         num_amis = len(list(session.resource("ec2").images.filter(Owners=["self"])))
-        table.append([region["RegionName"], num_instances, num_amis])
+        table.append([region, num_instances, num_amis])
     page_output(format_table(table, column_names=columns, max_col_width=args.max_col_width))
 
 parser = register_parser(top, help='Show an overview of AWS resources per region')
