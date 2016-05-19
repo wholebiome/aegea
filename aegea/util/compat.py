1a4df03583b8c3015570f0b2631388d071df562e
Andrey Kislyuk <kislyuk@gmail.com>
2016-05-19 13:21:31 -0700

    Fix linter error on Python 3

--- aegea/util/compat.py
+++ aegea/util/compat.py
@@ -7,7 +7,7 @@ USING_PYTHON2 = True if sys.version_info < (3, 0) else False
 if USING_PYTHON2:
     from StringIO import StringIO
     from repr import Repr
-    str = unicode
+    str = unicode # noqa
 else:
     from io import StringIO
     from reprlib import Repr
