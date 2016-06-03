967a63c06acbda683ab54067bb55e687dae7f2da
Andrey Kislyuk <kislyuk@gmail.com>
2016-06-03 13:58:48 -0700

    Fix test

--- aegea/build_ami.py
+++ aegea/build_ami.py
@@ -45,7 +45,7 @@ def build_image(args):
         args.hostname = "{}-{}".format(__name__.replace(".", "-").replace("_", "-"), int(time.time()))
         args.wait_for_ssh = True
         fields = "spot spot_price duration_hours iam_role subnet availability_zone use_dns cores min_mem_per_core_gb client_token"  # noqa
-        for field in fields:
+        for field in fields.split():
             setattr(args, field, None)
         instance = ec2.Instance(launch(args,
                                        user_data_commands=get_bootstrap_commands(),
