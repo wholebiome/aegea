5083870ad7b66c7bdac3b00c1f5f09a6ab97d8d9
Andrey Kislyuk <kislyuk@gmail.com>
2016-06-21 15:18:12 -0700

    Format timestamp; python3 test fixup

--- aegea/util/aws/_boto3_loader.py
+++ aegea/util/aws/_boto3_loader.py
@@ -5,21 +5,14 @@ class Loader:
 
     def __getattr__(self, attr):
         if attr == '__all__':
-            return list()
-        elif attr == '__package__':
-            return self.__package__
-        elif attr == '__path__':
-            return self.__path__
-        elif attr == '__file__':
-            return self.__file__
-        elif attr == '__loader__':
+            return list(self.cache[self.factory])
+        if attr == '__path__' or attr == '__loader__':
             return None
-        else:
-            if attr not in self.cache[self.factory]:
-                if self.factory == "client" and attr in self.cache["resource"]:
-                    self.cache["client"][attr] = self.cache["resource"][attr].meta.client
-                else:
-                    import boto3
-                    factory = getattr(boto3, self.factory)
-                    self.cache[self.factory][attr] = factory(attr)
-            return self.cache[self.factory][attr]
+        if attr not in self.cache[self.factory]:
+            if self.factory == "client" and attr in self.cache["resource"]:
+                self.cache["client"][attr] = self.cache["resource"][attr].meta.client
+            else:
+                import boto3
+                factory = getattr(boto3, self.factory)
+                self.cache[self.factory][attr] = factory(attr)
+        return self.cache[self.factory][attr]
