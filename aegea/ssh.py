79783ddc17d8a600d724b8ee04765c1e7d54d388
Andrey Kislyuk <kislyuk@gmail.com>
2016-08-31 11:30:51 -0700

    Expand aegea ssh help

--- aegea/ssh.py
+++ aegea/ssh.py
@@ -1,8 +1,23 @@
+"""
+Connect to an EC2 instance via SSH, by name or instance ID.
+
+Security groups, network ACLs, interfaces, VPC routing tables, VPC
+Internet Gateways, and internal firewalls for the instance must be
+configured to allow SSH connections.
+
+To facilitate SSH connections, ``aegea ssh`` resolves instance names
+to public DNS names assigned by AWS, and securely retrieves SSH host
+public keys from instance metadata before connecting. This avoids both
+the prompt to save the instance public key and the resulting transient
+MITM vulnerability.
+"""
+
 import os, sys, argparse, subprocess
 
 from . import register_parser
 from .util.aws import resolve_instance_id
 from .util.crypto import add_ssh_host_key_to_known_hosts
+from .util.printing import BOLD
 
 def ssh(args):
     from .util.aws.resources import ec2
@@ -17,6 +32,7 @@ def ssh(args):
     ssh_args = ["ssh", prefix + at + instance.public_dns_name] + args.ssh_args
     os.execvp("ssh", ssh_args)
 
-parser = register_parser(ssh, help="Connect to an instance")
+parser = register_parser(ssh, help="Connect to an instance", description=__doc__)
 parser.add_argument("name")
-parser.add_argument("ssh_args", nargs=argparse.REMAINDER)
+parser.add_argument("ssh_args", nargs=argparse.REMAINDER,
+                    help="Arguments to pass to ssh; please see " + BOLD("man ssh") + " for details")
