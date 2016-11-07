a31095a92182e0361280087cbed179470f8ba8a8
Andrey Kislyuk <kislyuk@gmail.com>
2016-11-07 15:37:53 -0800

    Arvados WIP

--- aegea/missions/arvados/deploy-arvados.sh
+++ aegea/missions/arvados/deploy-arvados.sh
@@ -10,32 +10,34 @@ fi
 
 # Restore protocol: install from scratch, restore RDS snapshot, dpkg reconfigure arvados-...-server (for migrations)
 
+export ARVADOS_INSTANCE=arvados-$(date "+%Y-%m-%d-%H-%M")
+export ARVADOS_SLURMCTL_HOST=$ARVADOS_INSTANCE
+
 aegea-build-ami-for-mission arvados arvados-$(date "+%Y-%m-%d-%H-%M")
 aegea-build-ami-for-mission arvados-worker arvwrkr-$(date "+%Y-%m-%d-%H-%M")
 
-host=arvados-$(date "+%Y-%m-%d-%H-%M")
-aegea launch $host --ami-tags AegeaMission=arvados --wait-for-ssh
+aegea launch $ARVADOS_INSTANCE --ami-tags AegeaMission=arvados --wait-for-ssh
 
-aegea zones update $PRIVATE_DNS_ZONE arvados "$host"
+aegea zones update $PRIVATE_DNS_ZONE arvados "$ARVADOS_INSTANCE.$PRIVATE_DNS_ZONE"
 
 export ARVADOS_ELB_INWARD_SG=aegea.launch
 export ARVADOS_ELB_OUTWARD_SG=http+https
-aegea elb create arvados-sso $host --dns-alias auth.$ARVADOS_HOSTNAME --security-groups $ARVADOS_ELB_INWARD_SG $ARVADOS_ELB_OUTWARD_SG --instance-port 2000
-aegea elb create arvados-api $host --dns-alias $ARVADOS_UUID_PREFIX.$ARVADOS_HOSTNAME --security-groups $ARVADOS_ELB_INWARD_SG $ARVADOS_ELB_OUTWARD_SG --instance-port 2001
-aegea elb create arvados-workbench $host --dns-alias workbench.$ARVADOS_HOSTNAME --security-groups $ARVADOS_ELB_INWARD_SG $ARVADOS_ELB_OUTWARD_SG  --instance-port 2002
-aegea elb create arvados-keepproxy $host --dns-alias keep.$ARVADOS_HOSTNAME --security-groups $ARVADOS_ELB_INWARD_SG $ARVADOS_ELB_OUTWARD_SG  --instance-port 25107
+aegea elb create arvados-sso $ARVADOS_INSTANCE --dns-alias auth.$ARVADOS_HOSTNAME --security-groups $ARVADOS_ELB_INWARD_SG $ARVADOS_ELB_OUTWARD_SG --instance-port 2000
+aegea elb create arvados-api $ARVADOS_INSTANCE --dns-alias $ARVADOS_UUID_PREFIX.$ARVADOS_HOSTNAME --security-groups $ARVADOS_ELB_INWARD_SG $ARVADOS_ELB_OUTWARD_SG --instance-port 2001
+aegea elb create arvados-workbench $ARVADOS_INSTANCE --dns-alias workbench.$ARVADOS_HOSTNAME --security-groups $ARVADOS_ELB_INWARD_SG $ARVADOS_ELB_OUTWARD_SG  --instance-port 2002
+aegea elb create arvados-keepproxy $ARVADOS_INSTANCE --dns-alias keep.$ARVADOS_HOSTNAME --security-groups $ARVADOS_ELB_INWARD_SG $ARVADOS_ELB_OUTWARD_SG  --instance-port 25107
 
 # keep-web aliases:
 #download.uuid_prefix.your.domain
 #collections.uuid_prefix.your.domain
 #*.collections.uuid_prefix.your.domain
-#aegea elb create arvados-keepweb $host --dns-alias keep.$ARVADOS_UUID_PREFIX.$ARVADOS_HOSTNAME --security-groups $ARVADOS_ELB_INWARD_SG $ARVADOS_ELB_OUTWARD_SG  --instance-port 
+#aegea elb create arvados-keepweb $ARVADOS_INSTANCE --dns-alias keep.$ARVADOS_UUID_PREFIX.$ARVADOS_HOSTNAME --security-groups $ARVADOS_ELB_INWARD_SG $ARVADOS_ELB_OUTWARD_SG  --instance-port 
 
-aegea zones update $DNS_ZONE "*.$ARVADOS_UUID_PREFIX.arvados" "$ARVADOS_UUID_PREFIX.arvados"
+aegea zones update $PUBLIC_DNS_ZONE "*.$ARVADOS_UUID_PREFIX.arvados" "$ARVADOS_UUID_PREFIX.arvados.$PUBLIC_DNS_ZONE"
 
-ARVADOS_SUPERUSER_TOKEN=$(aegea ssh ubuntu@$host "cd /var/www/arvados-api/current; sudo -u www-data RAILS_ENV=production bundle exec script/create_superuser_token.rb")
-aegea ssh ubuntu@$host "export PGUSER=$ARVADOS_DB_USERNAME PGPASSWORD=$ARVADOS_DB_PASSWORD PGHOST=$ARVADOS_DB_HOST; createuser --no-superuser --no-createrole --createdb --encrypted --no-password arvados_sso; createdb arvados_production -T template0 -E UTF8 -O $ARVADOS_DB_USERNAME; psql -c \"ALTER USER arvados_sso WITH UNENCRYPTED PASSWORD '$ARVADOS_DB_PASSWORD'\""
-aegea ssh ubuntu@$host "source /etc/profile; export ARVADOS_API_TOKEN=$ARVADOS_SUPERUSER_TOKEN; arv keep_service create --keep-service \"\$(cat /etc/keep.conf.json)\""
+ARVADOS_SUPERUSER_TOKEN=$(aegea ssh ubuntu@$ARVADOS_INSTANCE "cd /var/www/arvados-api/current; sudo -u www-data RAILS_ENV=production bundle exec script/create_superuser_token.rb")
+aegea ssh ubuntu@$ARVADOS_INSTANCE "export PGUSER=$ARVADOS_DB_USERNAME PGPASSWORD=$ARVADOS_DB_PASSWORD PGHOST=$ARVADOS_DB_HOST; createuser --no-superuser --no-createrole --createdb --encrypted --no-password arvados_sso; createdb arvados_production -T template0 -E UTF8 -O $ARVADOS_DB_USERNAME; psql -c \"ALTER USER arvados_sso WITH UNENCRYPTED PASSWORD '$ARVADOS_DB_PASSWORD'\""
+aegea ssh ubuntu@$ARVADOS_INSTANCE "source /etc/profile; export ARVADOS_API_TOKEN=$ARVADOS_SUPERUSER_TOKEN; arv keep_service create --keep-service \"\$(cat /etc/keep.conf.json)\""
 
 # /etc/arvados/crunch-dispatch-slurm/crunch-dispatch-slurm.yml - replace crunch dispatch token
 # Custom SSL: PEM=/tmp/stunnel.pem; openssl genrsa > $PEM; openssl req -new -x509 -key $PEM -subj / >> $PEM; stunnel -d 443 -r 2001 -p $PEM; ARVADOS_API_HOST_INSECURE=1
