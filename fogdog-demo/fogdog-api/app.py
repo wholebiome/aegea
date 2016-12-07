3858e102c523289aa5ee86963791204b7c9c9f27
Andrey Kislyuk <kislyuk@gmail.com>
2016-12-06 19:12:54 -0800

    WIP

--- fogdog-demo/fogdog-api/app.py
+++ fogdog-demo/fogdog-api/app.py
@@ -1,54 +1,69 @@
-import socket
-if socket.gethostname().startswith("serenity"):
-    import boto3, botocore, chalice.awsclient, chalice.deployer
-    class VPCLambdaClient(chalice.awsclient.TypedAWSClient):
-        def create_function(self, function_name, role_arn, zip_contents):
-            from aegea.util.aws import ensure_vpc, ensure_subnet, ensure_security_group
-            # type: (str, str, str) -> str
-            vpc = ensure_vpc()
-            kwargs = {
-                'FunctionName': function_name,
-                'Runtime': 'python2.7',
-                'Code': {'ZipFile': zip_contents},
-                'Handler': 'app.app',
-                'Role': role_arn,
-                'Timeout': 60,
-                'VpcConfig': {
-                    'SubnetIds': [ensure_subnet(vpc).id],
-                    'SecurityGroupIds': [ensure_security_group("aegea.launch", vpc).id]
-                }
-            }
-            client = self._client('lambda')
-            attempts = 0
-            while True:
-                try:
-                    response = client.create_function(**kwargs)
-                except botocore.exceptions.ClientError as e:
-                    code = e.response['Error'].get('Code')
-                    if code == 'InvalidParameterValueException':
-                        # We're assuming that if we receive an
-                        # InvalidParameterValueException, it's because
-                        # the role we just created can't be used by
-                        # Lambda.
-                        self._sleep(self.DELAY_TIME)
-                        attempts += 1
-                        if attempts >= self.LAMBDA_CREATE_ATTEMPTS:
-                            raise
-                        continue
-                    raise
-                return response['FunctionArn']
+# mysql --ssl-mode=REQUIRED --tls-version=TLSv1.2 --ssl-ca rds-ca-2015-root.pem --host fogdog-cluster.cluster-cepyx09zrohr.us-west-2.rds.amazonaws.com --user fogdog --password
 
-    chalice.awsclient.TypedAWSClient = VPCLambdaClient
-    chalice.deployer.TypedAWSClient = VPCLambdaClient
+import os, ssl, subprocess
+cafile = os.path.join(os.path.dirname(__file__), "chalicelib", "rds-ca-2015-root.pem")
+ssl_ctx = ssl.create_default_context(cafile=cafile)
 
 from chalice import Chalice
-
+from sqlalchemy import create_engine
+connect_args = dict(host="fogdog-cluster.cluster-cepyx09zrohr.us-west-2.rds.amazonaws.com",
+                    db="fogdog", user="fogdog", password="fogdogfogdog", ssl=ssl_ctx)
+engine = create_engine("mysql+pymysql://", connect_args=connect_args, echo=True)
 app = Chalice(app_name='fogdog-api')
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+class User(Base):
+     __tablename__ = 'users'
+     id = Column(Integer, primary_key=True)
+     name = Column(String(64))
+     fullname = Column(String(256))
+     password = Column(String(256))
+     def __repr__(self):
+         return "<User(name='%s', fullname='%s', password='%s')>" % (
+             self.name, self.fullname, self.password)
 
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    import boto3
+    res = None
+    engine.connect()
+    #session = Session()
+    #ed_user = User(name='ed', fullname='Ed Jones', password='edspassword')
+    #session.add(ed_user)
+    #session.commit()
+    #for instance in session.query(User).order_by(User.id):
+    #    res = str(instance)
+    '''
+    try:
+        res = Base.metadata.create_all(engine)
+    except Exception as e:
+        res = e
+    '''
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #return {'hello': 'fogdog', 'port': sock.connect_ex(('172.31.8.208', 22))}
+    return {'hello': 'fogdog', 'res': str(res), 'nw': str(boto3.client("ec2").describe_network_interfaces())}
+#, 'port': socket.gethostbyname(socket.gethostname()), 'res': str(res), 'nw': boto3.client("ec2").describe_network_interfaces()}
+
+@app.route('/load_gene_abun_dataset')
+def load_gene_abun_dataset():
+    return {}
+
+@app.route('/load_metabolite_abun_dataset')
+def load_metabolite_abun_dataset():
+    return {}
+
+@app.route('/load_otu_biom_dataset')
+def load_otu_biom_dataset():
+    return {}
+
+@app.route('/load_phylogenetic_tree')
+def load_phylogenetic_tree():
+    return {}
 
 
 # The view function above will return {"hello": "world"}
