d258937adb1ed54ef7d35df4fe1a53a3bc771abd
Andrey Kislyuk <kislyuk@gmail.com>
2016-12-20 12:15:29 -0800

    Batch WIP

--- aegea/batch.py
+++ aegea/batch.py
@@ -57,7 +57,7 @@ def create_compute_environment(args):
     batch_iam_role = ARN(service="iam", region="", resource="role/service-role/AWSBatchServiceRole")
     vpc = ensure_vpc()
     compute_resources = dict(type=args.compute_type,
-                             minvCpus=0, maxvCpus=256,
+                             minvCpus=0, desiredvCpus=8, maxvCpus=256,
                              instanceTypes=["optimal"],
                              subnets=[subnet.id for subnet in vpc.subnets.all()],
                              securityGroupIds=[ensure_security_group("aegea.launch", vpc).id],
@@ -83,12 +83,46 @@ def delete_compute_environment(args):
 parser = register_parser(delete_compute_environment, parent=batch_parser, help="Delete a Batch compute environment")
 parser.add_argument("name")
 
+def ensure_job_definition():
+    job_def = clients.batch.register_job_definition(jobDefinitionName="foo",
+                                                    type="container",
+                                                    containerProperties=dict(image="busybox", vcpus=1, memory=1024, command=["pwd"]))
+    return job_def["jobDefinitionArn"]
+
 def submit(args):
-    raise NotImplementedError()
+    if args.job_definition is None:
+        args.job_definition = ensure_job_definition()
+    return clients.batch.submit_job(jobName=args.name,
+                                    jobQueue=args.queue,
+                                    dependsOn=args.depends_on,
+                                    jobDefinition=args.job_definition,
+                                    parameters=args.parameters,
+                                    containerOverrides={})
 
 parser = register_parser(submit, parent=batch_parser, help="Submit a job to a Batch queue")
+parser.add_argument("name")
+parser.add_argument("--queue", required=True)
+parser.add_argument("--depends-on", nargs="+", default=[])
+parser.add_argument("--job-definition")
+parser.add_argument("--parameters", nargs="+", metavar="NAME=VALUE", type=lambda x: x.split("=", 1), default={})
 
 def terminate(args):
     raise NotImplementedError()
+
+def ls(args):
+    table = []
+    if args.queues is None:
+        args.queues = [q["jobQueueName"] for q in clients.batch.describe_job_queues()["jobQueues"]]
+    if args.status is None:
+        args.status = {'SUBMITTED', 'PENDING', 'RUNNABLE', 'STARTING', 'RUNNING', 'SUCCEEDED', 'FAILED'}
+    for queue in args.queues:
+        for status in args.status:
+            for job in clients.batch.list_jobs(jobQueue=queue, jobStatus=status)["jobSummaryList"]:
+                table.extend(clients.batch.describe_jobs(jobs=[job["jobId"]])["jobs"])
+    page_output(tabulate(table, args))
+
+parser = register_listing_parser(ls, parent=batch_parser, help="List Batch jobs")
+parser.add_argument("--queues", nargs="+")
+parser.add_argument("--status", nargs="+")
 
 parser = register_parser(terminate, parent=batch_parser, help="Terminate a Batch job")
