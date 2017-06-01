75a5a0099191ceb8198128611a452ab752e18ce6
Andrey Kislyuk <kislyuk@gmail.com>
2017-06-01 09:02:24 +0200

    Fix logic error in parameter naming

--- aegea/batch.py
+++ aegea/batch.py
@@ -98,7 +98,7 @@ def create_compute_environment(args):
                              ec2KeyPair=ssh_key_name)
     if args.ecs_container_instance_ami:
         compute_resources["imageId"] = args.ecs_container_instance_ami
-    elif args.ecs_container_instance_tags:
+    elif args.ecs_container_instance_ami_tags:
         # TODO: build ECS CI AMI on demand
         compute_resources["imageId"] = resolve_ami(**args.ecs_container_instance_tags)
     logger.info("Creating compute environment %s in %s", args.name, vpc)
